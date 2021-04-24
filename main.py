5159d9c5309ae3e3fbaf6def4baec3af16e0869a
MKrupa <mateuszkrupa123@wp.pl>
2021-04-24 14:42:16 +0200

    t

--- main.py
+++ main.py
@@ -1,27 +1,9 @@
+from Generator import Generator
 
-import random
-enter_list = []
-package = []
-package_list = []
-for a in range(0,9):
-    enter_list.append(random.randint(0,1))
+gene = Generator(5, 3)
 
-print(enter_list)
-current = 0
-inpack = 0
-for a in enter_list:
-        if enter_list[current] == 1:
-            package.append('111')
-        elif enter_list[current] == 0:
-            package.append('000')
-        inpack +=1
-        current += 1
-        if inpack == 7:
-            package_list.extend(package)
-            package_list.append("")
-            inpack = 0
-            package.clear()
+gene.multiple_bit(3)
 
+gene.show()
 
-
-print(package_list)
+gene.transfer()
